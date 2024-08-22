f7b77acc2aeb7a8ce026bb489d28559a69aa3c14
ariefnugraha <arief.nugraha@graveltechnology.com>
2024-08-22 08:38:23 +0700

    update cors 1

--- api/mixpanel.js
+++ api/mixpanel.js
@@ -1,19 +1,6 @@
-import { createProxyMiddleware } from "http-proxy-middleware";
-import cors from "cors";
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const mixpanelApiUrl = process.env.MIXPANEL_API;
-
-const corsOptions = {
-  origin: "https://staging.graveltechnology.com", // Ganti dengan domain yang Anda izinkan
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: [
-    "Origin",
-    "X-Requested-With",
-    "Content-Type",
-    "Accept",
-    "Authorization",
-  ],
-};
 
 const proxy = createProxyMiddleware({
   target: mixpanelApiUrl,
@@ -21,14 +8,38 @@ const proxy = createProxyMiddleware({
   pathRewrite: {
     "^/api/mixpanel": "", // Sesuaikan dengan rute API di Vercel
   },
+  onProxyReq: (proxyReq, req, res) => {
+    console.log("Proxying request to:", proxyReq.path);
+  },
+  onProxyRes: (proxyRes, req, res) => {
+    console.log("Proxy response headers:", proxyRes.headers);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+  },
 });
 
 export default function handler(req, res) {
-  cors(corsOptions)(req, res, () => {
-    if (req.method === "OPTIONS") {
-      res.status(200).end();
-      return;
-    }
-    return proxy(req, res);
-  });
+  console.log("Received request:", req.method, req.url);
+  if (req.method === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.status(200).end();
+    return;
+  }
+
+  return proxy(req, res);
 }
