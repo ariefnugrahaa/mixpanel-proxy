dce4f31b52a90faca3493684572ce61501a311e0
ariefnugraha <arief.nugraha@graveltechnology.com>
2024-08-22 08:00:54 +0700

    Handle cors

--- api/mixpanel.js
+++ api/mixpanel.js
@@ -9,8 +9,33 @@ const proxy = createProxyMiddleware({
   pathRewrite: {
     "^/api/mixpanel": "", // Sesuaikan dengan rute API di Vercel
   },
+  onProxyReq: (proxyReq, req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+  },
 });
 
 export default function handler(req, res) {
+  if (req.method === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.status(200).end();
+    return;
+  }
+
   return proxy(req, res);
 }
